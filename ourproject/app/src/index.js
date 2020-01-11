eb35419dd4e05bf3e657de28ca6a069ccddd5327
qqrsday <qqrsday@163.com>
2020-01-11 15:46:52 +0800

    0.4

--- ourproject/app/src/index.js
+++ ourproject/app/src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { HashRouter as Router, } from 'react-router-dom';
+//全局样式
+import './assets/css/bass.css';
+//引入外部样式
+import 'antd/dist/antd.css';
+//根组件
 import App from './App';
 
 import * as serviceWorker from './serviceWorker';
-
-import { HashRouter } from 'react-router-dom';
-
+//渲染
 ReactDOM.render(
-    <HashRouter>
-    <App />
-    </HashRouter>
+    <Router>
+        <App />
+    </Router>
     ,
     document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
